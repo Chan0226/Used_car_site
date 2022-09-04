3fa97fb92d9e395845e480a3b88cea451a4f7186
김병찬 <84282676+Chan0226@users.noreply.github.com>
2022-09-04 16:18:14 +0900

    Add files via upload

--- cartdto/UserDto.java
+++ cartdto/UserDto.java
@@ -1,5 +1,6 @@
 package data.dto.cart;
 
+import java.sql.Date;
 import java.sql.Timestamp;
 
 
@@ -7,7 +8,13 @@ public class UserDto {
 	private int num; // 번호
 	private String id; // 아이디
 	private String password;
+	private String name;
+	private Date birthday;
+	private String sex;
+	private String emailid;
+	private String emailaddr;
 	private Timestamp createDate;
+
 	public int getNum() {
 		return num;
 	}
@@ -31,6 +38,36 @@ public class UserDto {
 	}
 	public void setCreateDate(Timestamp createDate) {
 		this.createDate = createDate;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getSex() {
+		return sex;
+	}
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+	public Date getBirthday() {
+		return birthday;
+	}
+	public void setBirthday(Date birthday) {
+		this.birthday = birthday;
+	}
+	public String getEmailid() {
+		return emailid;
+	}
+	public void setEmailid(String emailid) {
+		this.emailid = emailid;
+	}
+	public String getEmailaddr() {
+		return emailaddr;
+	}
+	public void setEmailaddr(String emailaddr) {
+		this.emailaddr = emailaddr;
 	}
 	
 }
